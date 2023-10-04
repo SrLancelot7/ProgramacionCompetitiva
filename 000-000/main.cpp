5a0032d4e4b832d2d56e456a810270c3da340cad
SrLancelot7 <136867002+SrLancelot7@users.noreply.github.com>
2023-10-04 09:47:08 -0300

    Add files via upload

--- 000-000/main.cpp
+++ 000-000/main.cpp
@@ -1,49 +1,49 @@
 #include<stdio.h>
 #include<vector>
 #include<stack>
-#include<map>
-#include<iostream>
-using namespace std;
-//antes de compilar cualquier cosa, acuerdate de guardar lo que cambiaste
+#include <algorithm>
+typedef std::vector<int> vi;
+
 
 class Grafo {
-public :
-    int V ; // numero de vertices
-    vector < vector <int > > adjMatrix ;
+    public :
+        int V ;
+        std::vector <vi> adjList ;
 
-    Grafo (int vertices ) {
-        V = vertices ;
-        adjMatrix.resize (V , vector <int>(V ,0));
-    }
-    void addEdge ( int u , int v ) {
-        adjMatrix [ u ][ v ] = 1; // 1 para arista
-        adjMatrix [ v ][ u ] = 1; // Grafo no dirigido
-    }
-};
-/*
-void DFS (int startVertex ) {
-    vector <bool > visited (V , false ) ;
-    stack <int > stack ;
-
-    stack.push( startVertex ) ;
-    visited[startVertex] = true ;
-
-    while (! stack . empty () ) {
-        int currentVertex = stack.top ();
-        stack.pop ();
- 
-        for (int neighbor : adjList [ currentVertex ])
-        {
-            if (! visited [ neighbor ]) {
-            stack . push ( neighbor ) ;
-            visited [ neighbor ] = true ;
-            }
+        Grafo (int vertices ){
+            V = vertices ;
+            adjList.resize ( V ) ;
         }
-    }
- }
-*/
-int main()
-{
+        void addEdge(int u , int v ) {
+            adjList [ u ]. push_back ( v ) ;
+            adjList [ v ]. push_back ( u ) ;
+        }
+        int DFS ( int startVertex ) {
+            int visitaciones = 0;
+            std::vector<bool> visited(V , false);
+            std::stack<int> stack;
+
+            stack . push ( startVertex ) ;
+            visited [ startVertex ] = true ;
+
+            while (! stack . empty () ) {
+                int currentVertex = stack . top () ;
+                stack . pop () ;
+
+                for (int neighbor : adjList [ currentVertex ]){
+                    if (! visited [ neighbor ]) {
+                        stack . push ( neighbor ) ;
+                        visited [ neighbor ] = true ;
+                    }
+                    visitaciones++;
+                }
+            }
+            return visitaciones;
+        }
+};
+
+int main () {
+
     #ifndef ONLINE_JUDGE
     
         // For getting input from input.txt file
@@ -54,38 +54,21 @@ int main()
     
     #endif
 
-    int t,N,M,tmp;
+    int t,n,V,A,a,b;
     scanf("%d",&t);
-    while (t--)
-    {
-        scanf("%d %d",&N,&M);
-        map<pair<int, int>,int> valor;
-        for (int i = 0; i < N; i++){
-            for (int j = 0; j < M; j++){
-                scanf("%d",&tmp);
-                switch (tmp){
-                case 0:
-                    valor[make_pair(i,j)] = 0;
-                    break;                    
-                case 1:
-                    valor[make_pair(i,j)] = 1;
-                    break;
-                case 2:
-                    valor[make_pair(i,j)] = 2;
-                    break;
-                }
-            }
+    while (t--){
+        scanf("%d",&n);
+        scanf("%d %d",&V,&A);
+        Grafo laberinto (V);
+        while (A--){
+            scanf("%d %d",&a,&b);
+            if(std::find(laberinto.adjList[a].begin(),laberinto.adjList[a].end(),b) == laberinto.adjList[a].end())
+                laberinto.addEdge(a,b);
         }
-        map<pair<int,int>,int>::iterator it = valor.begin();
-
-        while (it != valor.end()){
-            printf("key: (%d,%d) , value: %d\n",it->first.first,it->first.second,it->second);
-            it++;
-        }
-
-        
+        printf("%d\n",laberinto.DFS(n));
     }
     
 
+    
     return 0;
 }
