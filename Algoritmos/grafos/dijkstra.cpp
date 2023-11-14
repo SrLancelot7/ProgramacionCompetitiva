4d6f49f6c921eb675c41a17f174ab5124a8e34f1
SrLancelot7 <sleon2001712@gmail.com>
2023-11-14 20:52:29 -0300

    modified:   Algoritmos/grafos/dijkstra 	modified:   Algoritmos/grafos/dijkstra.cpp

--- Algoritmos/grafos/dijkstra.cpp
+++ Algoritmos/grafos/dijkstra.cpp
@@ -9,6 +9,7 @@ std::vector<int> djks_dist;
 
 void addEdge(int u, int v,int p){
     AdjList[u].push_back(std::make_pair(v,p));
+    AdjList[v].push_back(std::make_pair(u,p));
 }
 void djks(int u,int p){
     djks_dist[u] = p;
@@ -28,15 +29,10 @@ int main(){
     djks_dist.assign(V,MAX);
 
     addEdge(0,1,5);
-    addEdge(1,0,5);
     addEdge(0,2,6);
-    addEdge(2,0,6);
     addEdge(1,3,2);
-    addEdge(3,1,2);
     addEdge(3,4,3);
-    addEdge(4,3,3);
     addEdge(2,3,0);
-    addEdge(3,2,0);
 
     for (int i = 0; i < AdjList.size(); i++)
     {
@@ -48,7 +44,7 @@ int main(){
         printf("\n\n");
     }
 
-    djks(0,0);
+    djks(3,0);
     for (int i = 0; i < V; i++)
         printf("%d ",djks_dist[i]);
     printf("\n");
