dd4248a38cd05e171e8927b5f49071e34a544d3d
SrLancelot7 <sleon2001712@gmail.com>
2023-12-01 18:04:41 -0300

    new file:   "000-000/Estrategia de Mudanzas en la Ciudad Futbol\303\255stica/input.txt" 	new file:   "000-000/Estrategia de Mudanzas en la Ciudad Futbol\303\255stica/main" 	new file:   "000-000/Estrategia de Mudanzas en la Ciudad Futbol\303\255stica/main.cpp" 	new file:   "000-000/Estrategia de Mudanzas en la Ciudad Futbol\303\255stica/main.exe" 	new file:   "000-000/Estrategia de Mudanzas en la Ciudad Futbol\303\255stica/otro.cpp" 	new file:   "000-000/Estrategia de Mudanzas en la Ciudad Futbol\303\255stica/output.txt"

---
+++ 000-000/Estrategia de Mudanzas en la Ciudad Futbolística/main.cpp
@@ -0,0 +1,55 @@
+#include<vector>
+#include<stdio.h>
+typedef std::vector<int> vi;
+//************************************
+//NO FUNCIONA
+//************************************
+std::vector<vi> AdjList;
+std::vector<char> futbol;
+
+void addEdge(int u, int v){
+    AdjList[u].push_back(v);
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    
+        // For getting input from input.txt file
+        freopen("input.txt", "r", stdin);
+    
+        // Printing the Output to output.txt file
+        freopen("output.txt", "w", stdout);
+    
+    #endif
+
+    int V,in,f;
+    char e;
+    scanf("%d",&V);
+    AdjList.assign(V, vi());
+    futbol.assign(V, 'a');
+
+    for (int i = 0; i < V; i++){
+        scanf(" %c %d",&e,&in);
+        futbol[i] = e;
+        while (in--){
+            scanf("%d",&f);
+            addEdge(i,f-1);
+        }
+    }
+
+    int total;
+    for (int i = 0; i < (int)AdjList.size(); i++){
+        total = 0;
+        for (int j = 0; j < (int)AdjList[i].size(); j++)
+        {
+            int v = AdjList[i][j];
+            if(futbol[v] != futbol[i])
+                total++;
+        }
+        if(total > (int)AdjList[i].size()/2 && (int)AdjList[i].size() != 0)
+            printf("%d ",i+1);
+    }
+    
+    return 0;
+}
