b06b0d4b275a3da8998a611ca7a97d24466dc383
SrLancelot7 <sleon2001712@gmail.com>
2023-11-30 23:59:35 -0300

    new file:   000-000/Organizando el Paseo de Curso/compare.txt 	new file:   000-000/Organizando el Paseo de Curso/input.txt 	new file:   000-000/Organizando el Paseo de Curso/main 	new file:   000-000/Organizando el Paseo de Curso/main.cpp 	new file:   000-000/Organizando el Paseo de Curso/main.exe 	new file:   000-000/Organizando el Paseo de Curso/output.txt

---
+++ 000-000/Organizando el Paseo de Curso/main.cpp
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<algorithm>
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    
+        // For getting input from input.txt file
+        freopen("input.txt", "r", stdin);
+    
+        // Printing the Output to output.txt file
+        freopen("output.txt", "w", stdout);
+    
+    #endif
+
+    int n;
+    bool cumple;
+    scanf("%d",&n);
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d",&arr[i]);
+    
+    std::sort(arr,arr+n);
+    do
+    {
+        cumple = true;
+        if(n == 1 && arr[0] < 18)
+            cumple = false;
+        else
+            for (int i = 0; i < n; i++)
+                if(arr[i] < 18)
+                    if((arr[i-1] < 18 || i==0) && (arr[i+1] < 18 || i== n-1))
+                        cumple = false;
+        if(cumple){
+            for (int i = 0; i < n; i++)
+                printf("%d ",arr[i]);
+            printf("\n");
+        }
+        
+        
+    } while (std::next_permutation(arr,arr+n));
+    
+    
+
+    return 0;
+}
